9a036b4e6c53342d8468dd019db8e4ded1591966
Nishith <nishithsarma@gmail.com>
2024-11-04 13:52:10 -0600

    Refinement

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/Login.css";
+import "../styles/login.css";
 import lock from "../assets/lock.png";
 
 const Login = () => {
