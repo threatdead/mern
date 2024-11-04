70af2ff6b87d161a12f0975ccb2eeded9d5312c1
Nishith <nishithsarma@gmail.com>
2024-11-04 14:37:21 -0600

    Refine server.js

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,10 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/claims', claimRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) =>{
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
